7ab25c99ebb91d9de482ba2c2ec668742333b9f6
xjz <1138545951@qq.com>
2020-03-25 21:30:49 +0800

    上传了项目文件

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour
+{
+
+    private int isforward;//标记摄像机的移动方向
+    //记录两个手指的旧位置
+    private Vector2 oposition1 = new Vector2();
+    private Vector2 oposition2 = new Vector2();
+
+    Vector2 m_screenPos = new Vector2(); //记录手指触碰的位置
+
+    void Start()
+    {
+        Input.multiTouchEnabled = true;//开启多点触碰
+    }
+
+    void Update()
+    {
+        if (Input.touchCount <= 0)
+            return;
+        if (Input.touchCount == 1) //单点触碰移动摄像机
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+                m_screenPos = Input.touches[0].position;   //记录手指刚触碰的位置
+            if (Input.touches[0].phase == TouchPhase.Moved) //手指在屏幕上移动，移动摄像机
+            {
+                transform.Translate(new Vector3(-Input.touches[0].deltaPosition.y * Time.deltaTime*1.5f , 0, Input.touches[0].deltaPosition.x * Time.deltaTime*1.5f),Space.World);
+                
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CameraTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraTest : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject cameraTest_text;
+    Text cameratext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.Find("Main Camera");
+        cameraTest_text=GameObject.Find("CameraTest_text");
+        cameratext = cameraTest_text.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameratext.text = "x:" + mainCamera.transform.position.x+ "\ny:"+ mainCamera.transform.position.y+"\nz:" + mainCamera.transform.position.z;
+    }
+}
